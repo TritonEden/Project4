5f4ba8b5e20864ec2f26474e593b3016843b1156
TritonEden <teden@vols.utk.edu>
2023-03-31 13:26:17 -0400

    got all of the outputs working except output1. I think its because input1 is the only input that has a start tile that isn't 0 0.

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -20,25 +20,26 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < tNum; i++) {
 		cin >> tName[i] >> tCost[i];
 	}
+
 	cin >> tRows >> tCols;
 	// matrix to input tile values
-	char tData[tRows][tCols];
-	for (int i = 0; i < tRows; i++) {
-		for (int j = 0; j < tCols; j++) {
-			cin >> tData[i][j];
-		}
+	char tData[tRows * tCols];
+	for (int i = 0; i < (tRows * tCols); i++) {
+		cin >> tData[i];
 	}
 	cin >> startRow >> startCol;
 	cin >> endRow >> endCol;
 
+	/*for (int i = 0; i < tNum; i++) {
+		cout << tName[i] << " " << tCost[i] << endl;
+	}*/
+
     // changing the table of characters to actual numerical weights of each tile
     int tNums[tRows * tCols];
-    for (int i = 0; i < tCols; i++) {
-        for (int j = 0; j < tRows; j++) {
-            for (int k = 0; k < tNum; k++){
-                if (tName[k] == tData[i][j]) {
-                    tNums[j + (i * 10)] = tCost[k];
-                }
+	for (int i = 0; i < (tRows * tCols); i++) {
+        for (int k = 0; k < tNum; k++){
+            if (tName[k] == tData[i]) {
+                tNums[i] = tCost[k];
             }
         }
     }
@@ -56,20 +57,18 @@ int main(int argc, char *argv[]) {
 	// visited array initialized to false
 	int visited[tRows * tCols];
 
+	//cout << "pre initialization" << endl;
+
 	// initialize arrays
-	for (unsigned char i = 0; i < (tRows * tCols); i++) {
+	for (int i = 0; i < (tRows * tCols); i++) {
 		distance[i] = 999;
 		backL[i] = i;
 		visited[i] = 0;
 	}
 
 	// set distance and backlink of start node manually
-	distance[startRow + startCol * 10] = tNums[startRow + startCol * 10];
-	backL[0] = -1;
-
-	// making vectors to print the gradescripts
-	vector<int> outputRow;
-	vector<int> outputCol;
+	distance[startRow + startCol * tCols] = tNums[startRow + startCol * tCols];
+	backL[startRow + startCol * tCols] = -1;
 
 	// making values to check for nearest node
 	int minValue;
@@ -79,9 +78,9 @@ int main(int argc, char *argv[]) {
 
 	for (int i = 0; i < tRows * tCols; i++) {
 		// get minimum reachable univisited
-		minValue = 99;
+		minValue = 999;
 		minNode = 0;
-		for (int j = 0; j < (tRows * tCols) - 1; j++) {
+		for (int j = 0; j < tRows * tCols; j++) {
 			if (visited[j] == 0 && distance[j] < minValue) {	
 				minValue = distance[j];
 				minNode = j;
@@ -99,40 +98,56 @@ int main(int argc, char *argv[]) {
 				backL[minNode+1] = minNode;
 			}
 		}
-		if (distance[minNode+10] > tNums[minNode+10] + distance[minNode]) {
-			if (minNode+10 < (tCols * 10)) {
-				distance[minNode+10] = tNums[minNode+10] + distance[minNode];
-				backL[minNode+10] = minNode;
+		if (distance[minNode+tCols] > tNums[minNode+tCols] + distance[minNode]) {
+			if (minNode+tCols < (tCols * tCols)) {
+				distance[minNode+tCols] = tNums[minNode+tCols] + distance[minNode];
+				backL[minNode+tCols] = minNode;
 			}
 		}
 	}
-	
+
 	// outputing to match the grade scripts
-	cout << distance[endRow + (endCol * 10)] - tNums[endRow + (endCol * 10)] << endl;
-	int back = endRow + (endCol * 10);
+	cout << distance[endRow + (endCol * tCols)] - tNums[endRow + (endCol * tCols)] << endl;
+	int back = endRow + (endCol * tCols);
 	int it = 0;
 	// create array to store location of tiles traveled through
 	int tFinal[tRows*tCols];
+	int outputR[tRows*tCols];
+	int outputC[tRows*tCols];
+	for (int i = 0; i < tRows*tCols; i++) {
+		outputR[i] = 9999;
+		outputC[i] = 9999;
+	}
 	int tMath;
-	for (unsigned char i = 0; i < (tRows * tCols); i++) {
+	for (int i = 0; i < (tRows * tCols); i++) {
 		tFinal[i] = 10000;
 	}
-
 	while (backL[back] != -1) {
 		tFinal[it] = back;
 		back = backL[back];
 		it++;
 	}
-	for (int i = 0; i < tRows*tCols; i++) {
+	//cout << endRow << " " << endCol << endl;
+	outputR[0] = endRow;
+	outputC[0] = endCol;
+	for (int i = 1; i < tRows*tCols; i++) {
 		if(tFinal[i] != 10000) {
-			tMath = tFinal[i]/10;
-			cout << tFinal[i] - (10*tMath) << " " << tMath << endl;
+			tMath = tFinal[i]/tCols;
+			//cout << tMath << " " << tFinal[i] - (tCols*tMath) << endl;
+			outputR[i] = tMath;
+			outputC[i] = tFinal[i] - (tCols*tMath);
 		}
 		else {
-			i = 1000;
+			i = 999999;
 		}
 	}
-	cout << "0 0" << endl;
+	cout << startRow << " " << startCol << endl;
+	for (int i = tRows*tCols - 1; i > 0; i--) {
+		if (outputR[i] != 9999 && outputC[i] != 9999) {
+			cout << outputR[i] << " " << outputC[i] << endl;
+		}
+	}
+	cout << endRow << " " << endCol << endl;
 
 	// checking stuff
 
@@ -153,7 +168,8 @@ int main(int argc, char *argv[]) {
 	cout << endl;*/
 
 	// checking final distance vector
-	/* 	
+
+	/*
 	int inc = 0;
 	for (int i = 0; i < tRows * tCols; i++) {
 		if (inc == tCols) {
@@ -163,8 +179,8 @@ int main(int argc, char *argv[]) {
 		cout << distance[i] << " ";
 		inc++;
 	}
-	cout << endl;*/
-	
+	cout << endl;
+	*/
 
     // input testing
 	/*cout << tNum << endl;
@@ -173,14 +189,20 @@ int main(int argc, char *argv[]) {
 		cout << tName[i] << " " << tCost[i] << endl;
 	}
 	cout << tRows << " " << tCols << endl;
-	for (unsigned char i = 0; i < tRows; i++) {
-		for (unsigned char j = 0; j < tCols; j++) {
-			cout << tData[i][j] << " ";
+	int inc = 0;
+	for (int i = 0; i < tRows * tCols; i++) {
+		if (inc == tCols) {
+			cout << endl;
+			inc = 0;
 		}
-		cout << endl;
+		cout << tData[i] << " ";
+		inc++;
 	}
+	cout << endl;
 	cout << startRow << " " << startCol << endl;
-	cout << endRow << " " << endCol << endl;*/
+	cout << endRow << " " << endCol << endl;
 
-	return 0;
+	return 0;*/
 }
+
+
